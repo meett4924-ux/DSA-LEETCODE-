3d2d7ba3198b5130c489787e1afab431f1763473
Parmar Meet Sanjaykumar <meett4924@gmail.com>
2026-08-25 21:24:44 +0530

    Time: 4 ms (1.58%), Space: 9.9 MB (8.65%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() % 2 != 0) return false;
+
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(') {
+                st.push(')');
+            } else if (c == '{') {
+                st.push('}');
+            } else if (c == '[') {
+                st.push(']');
+            } else {
+                if (st.empty() || st.top() != c) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
